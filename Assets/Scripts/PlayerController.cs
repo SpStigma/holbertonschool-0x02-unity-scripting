a899821f227843dc6de164ce1cc0586eef27ea3c
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-15 00:09:21 +0200

    Task1: Move the Ball.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		float movementHorizontal = Input.GetAxis("Horizontal");
+		float movementVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(movementHorizontal, 0, movementVertical);
+
+		rb.velocity = new Vector3(movement.x * speed, rb.velocity.y, movementVertical * speed);
+	}
+}
