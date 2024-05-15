c55d91cd41983a8223cd47121701be2aaf2a972a
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2024-05-15 18:57:23 +0200

    Task 4 Coin collecting.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed;
 	private Rigidbody rb;
+	private int score;
 
 	void Start()
 	{
@@ -20,5 +21,16 @@ public class PlayerController : MonoBehaviour
 		Vector3 movement = new Vector3(movementHorizontal, 0, movementVertical);
 
 		rb.velocity = new Vector3(movement.x * speed, rb.velocity.y, movementVertical * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Pickup"))
+		{
+			score += 1;
+			Debug.Log("score: " + score);
+			Destroy(other.gameObject);
+			
+		}
 	}
 }
